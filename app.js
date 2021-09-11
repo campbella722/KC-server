6413d8442c7d4371bdf6cb04397804c0bf308c23
unknown <arianne.domka@gmail.com>
2021-09-11 19:58:57 -0400

    updated models and associations

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const User = sequelize.define("Guest", {
     lastname: {
         type: DataTypes.STRING
     },
-    username: {
+    email: {
         type: DataTypes.STRING
     },
     password: {
@@ -27,17 +27,98 @@ const User = sequelize.define("Guest", {
     }
 })
 
+const Profile = sequelize.define("Profile", {
+    address: {
+        type: DataTypes.STRING
+    },
+    city: {
+        type: DataTypes.STRING
+    },
+    state: {
+        type: DataTypes.STRING
+    },
+    zipcode: {
+        type: DataTypes.STRING
+    },
+    phone: {
+        type: DataTypes.STRING
+    }
 
+})
 
 /*one to one rel */
-
+User.hasOne(Profile, {
+    onDelete: 'CASCADE'
+}); //user.getProfile => get profile associated with id/user
+Profile.belongsTo(User);
 
 /*one to many rel */
+const Order = sequelize.define("Order", {
+    shippingAddress: {
+        type: DataTypes.STRING
+    },
+    shippingCity: {
+        type: DataTypes.STRING
+    },
+    shippingState: {
+        type: DataTypes.STRING
+    },
+    shippingZipcode: {
+        type: DataTypes.STRING
+    },
+    shippingPhone: {
+        type: DataTypes.STRING
+    },
+    requestByDate: {
+        type: DataTypes.DATE
+    },
+    subtotal: {
+        type: DataTypes.DECIMAL
+    }
+})
+User.hasMany(Order);
+Order.belongsTo(User);
+
+const Item = sequelize.define("Item", {
+    batchSize: {
+        type: DataTypes.STRING
+    },
+    batchFlavor: {
+        type: DataTypes.STRING
+    },
+    batchNumber: {
+        type: DataTypes.DECIMAL
+    },
+    itemCost: {
+        type: DataTypes.DECIMAL
+    }
+})
+
+Order.hasMany(Item);
+Item.belongsTo(Order);
+
+// const Status = sequelize.define("Status", {
+//     isReceived: {
+//         type: DataTypes.BOOLEAN
+//     },
+//     isInProgress: {
+//         type: DataTypes.BOOLEAN
+//     },
+//     isInTransit: {
+//         type: DataTypes.BOOLEAN
+//     },
+//     isCompleted: {
+//         type: DataTypes.BOOLEAN
+//     },
+//     isCancelled: {
+//         type: DataTypes.BOOLEAN
+//     }
+// })
+// Order.hasOne(Status);
+// Status.belongsTo(Order);
 
 
 /*Many to many rel*/
-
-
 
 
 ;((async () => {
@@ -47,8 +128,35 @@ const User = sequelize.define("Guest", {
     //     } catch (error) {
     //     console.error('Unable to connect to the database:', error);
     //     }
-    await sequelize.sync(/*{
+    await sequelize.sync({
         force: true
-    }*/);
+    });
+
+    let my_user = await User.create(
+        {
+            firstname: "Arianne",
+            lastname: "Domka",
+            email:"user@email.com",
+            password: "pw1234"
+        })
+    let my_profile = await Profile.create({
+        address: "123 Main St",
+        city: "Chicago",
+        state: "IL",
+        zipcode: "09876",
+        phone:"123456789"
+    })
+    console.log(await my_user.getProfile())
+    await my_user.setProfile(my_profile)
+    console.log(await my_user.getProfile())
+
+
+    let resultUser = await User.findOne({
+        where: {
+            id: 1
+        }
+    })
+    console.log(await resultUser.getProfile())
+
 })());
 
